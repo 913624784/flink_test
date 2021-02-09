fefe486bd53f9e91e2aa9936962516fe6abe047f
qizhiwei <qizhiwei@qiyi.com>
2021-02-09 17:42:25 +0800

    add test demo

--- src/main/scala/com/qzw/test/state/StateTest.scala
+++ src/main/scala/com/qzw/test/state/StateTest.scala
@@ -2,10 +2,11 @@ package com.qzw.test.state
 
 import com.qzw.test.source.SourceTest.SensorReading
 import com.qzw.test.window.MyReduce
-import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.api.common.functions.{RichFlatMapFunction, RichMapFunction}
 import org.apache.flink.api.common.state.{ListState, ListStateDescriptor, MapState, MapStateDescriptor, ReducingState, ReducingStateDescriptor, ValueState, ValueStateDescriptor}
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.util.Collector
 
 import java.util
 
@@ -26,10 +27,43 @@ object StateTest {
         SensorReading(sensor(0).trim, sensor(1).trim.toLong, sensor(2).trim.toDouble)
       })
 
+    //对温度跳变超过 10 度报警，不考虑时间戳
+    val alertStream = ds
+      .keyBy(_.id)
+      //      .flatMap(new Alert(10.0))
+
+      .flatMapWithState[(String, Double, Double), Double]({
+        case (data: SensorReading, None) => (List.empty, Some(data.temperature))
+        case (data: SensorReading, lastTemp: Some[Double]) =>
+          val diff = (data.temperature - lastTemp.get).abs
+          if (diff > 10.0) {
+            (List((data.id, lastTemp.get, data.temperature)), Some(data.temperature))
+          } else {
+            (List.empty, Some(data.temperature))
+          }
+      })
+    alertStream.print("alert")
     env.execute("StateTest")
   }
 }
 
+//自定义报警 (有个 bug 初始温度是 0.0 第一条数据一定会输出)
+class Alert(threshold: Double) extends RichFlatMapFunction[SensorReading, (String, Double, Double)] {
+  //保存上一次的温度
+  lazy val lastTemp: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", classOf[Double]))
+
+  override def flatMap(value: SensorReading, out: Collector[(String, Double, Double)]): Unit = {
+    val lastV = lastTemp.value()
+
+    val diff = (value.temperature - lastV).abs
+    if (diff > threshold) {
+      out.collect((value.id, lastV, value.temperature))
+    }
+    lastTemp.update(value.temperature)
+  }
+}
+
+//keyed state 测试，必须定义在 richFunction 中 获取运行时上下文
 class MyRichMapper extends RichMapFunction[SensorReading, String] {
 
   lazy val valueState: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("valueState", classOf[Double]))
